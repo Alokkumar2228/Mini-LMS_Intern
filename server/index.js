40959e44cf5f443c5d4b43cea5b48a8d3971f0a1
Alokkumar2228 <141926450+Alokkumar2228@users.noreply.github.com>
2025-08-02 03:28:24 +0530

    url-added

--- server/index.js
+++ server/index.js
@@ -15,13 +15,25 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true
-  }
-));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+    "https://mini-lms-intern.vercel.app" 
+  ];
+
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }));
+
 
 connectDB();
 
